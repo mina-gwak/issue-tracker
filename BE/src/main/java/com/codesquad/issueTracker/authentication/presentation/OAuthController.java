4bba68e01a823553b4d695a198ca6b684e9150be
leejohy-0223 <leejohy@naver.com>
2022-06-15 16:21:12 +0900

    feat: controller에 로그인 로직 구현 (mina-qwak/issue-tracker#1)
    - Interceptor 적용을 위한 API명 변경

--- BE/src/main/java/com/codesquad/issueTracker/authentication/presentation/OAuthController.java
+++ BE/src/main/java/com/codesquad/issueTracker/authentication/presentation/OAuthController.java
@@ -3,26 +3,33 @@ package com.codesquad.issueTracker.authentication.presentation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codesquad.issueTracker.authentication.application.OAuthService;
+import com.codesquad.issueTracker.authentication.presentation.dto.OAuthLoginTokenResponse;
 import com.codesquad.issueTracker.authentication.presentation.dto.OAuthorizationLoginUrlResponse;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/oauth")
 @RestController
 public class OAuthController {
 
     private final OAuthService oAuthService;
 
-    @GetMapping("/authorization/github")
+    @GetMapping("/github")
     public ResponseEntity<OAuthorizationLoginUrlResponse> githubAuthorizationUrl() {
-        return ResponseEntity
-            .ok(new OAuthorizationLoginUrlResponse(oAuthService.getAuthorizationUrl()));
+        return ResponseEntity.ok(new OAuthorizationLoginUrlResponse(oAuthService.getAuthorizationUrl()));
     }
 
-
+    @GetMapping("/login/github/callback")
+    public ResponseEntity<OAuthLoginTokenResponse> oAuthLogin(@RequestParam String code) {
+        OAuthLoginTokenResponse loginToken = oAuthService.login(code);
+        return ResponseEntity.ok(loginToken);
+    }
 
 }
