370247fff21a6cad75c7dc461a13c63295b7940f
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-06-17 04:59:32 +0900

    feat: 로딩 페이지 구현  (mina-gwak/issue-tracker#8)

---
+++ FE/src/pages/Loading/Loading.style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+import { pageContentCenter } from '@assets/styles/mixin';
+
+export const Wrapper = styled.div`  
+  ${pageContentCenter}
+`;
--- FE/src/pages/Loading/index.tsx
+++ FE/src/pages/Loading/index.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import FadeLoader from 'react-spinners/FadeLoader';
+
+import * as S from '@pages/Login/Login.style';
 
 const Loading = () => {
-  return <div></div>;
+  return (
+    <S.Wrapper>
+      <FadeLoader color='#007aff' height={25} width={6} radius={5} margin={10} />
+    </S.Wrapper>
+  );
 };
 
 export default Loading;
