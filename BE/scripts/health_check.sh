ec700023ceeda4eb5f517450cfa4048ef48124ad
leejohy-0223 <leejohy@naver.com>
2022-06-16 15:09:02 +0900

    feat: 배포 script 추가
    - run_new_was.sh : switch port(현재 nginx와 연결되지 않은 port)를 대상으로 새로운 was를 실행한다.
    - health_check.sh : 앞서 띄운 was를 대상으로 health check를 수행한다.
    - switch : health_check가 완료될 경우에만 nginx에서 proxy port를 변경하도록 하는 함수를 포함한다.

---
+++ BE/scripts/health_check.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+CURRENT_PORT=$(cat /home/ec2-user/config/service_url.inc | grep -Po '[0-9]+' | tail -1)
+SWITCH_PORT=0
+
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+  SWITCH_PORT=8082
+else
+  SWITCH_PORT=8081
+fi
+
+echo "> Start health check of WAS at 'http://127.0.0.1:${SWITCH_PORT}' ..."
+
+for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
+do
+    echo "> #${RETRY_COUNT} trying..."
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  http://127.0.0.1:${SWITCH_PORT}/api/oauth/github)
+    if [ ${RESPONSE_CODE} -eq 200 ]; then
+        echo "> New WAS successfully running"
+        switch_proxy
+        exit 0
+    elif [ ${RETRY_COUNT} -eq 10 ]; then
+        echo "> Health check failed. Kill WAS running at ${SWITCH_PORT}"
+        TARGET_PID=$(lsof -Fp -i TCP:${SWITCH_PORT} | grep -Po 'p[0-9]+')
+        sudo kill ${TARGET_PID}
+        exit 1
+    fi
+    sleep 10
+done
