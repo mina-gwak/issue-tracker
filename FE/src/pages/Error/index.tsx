4c28646e52caf05c21a4ab15dff24cc05c66f23b
mina-gwak <mina.gwak@kakao.com>
2022-06-17 05:08:45 +0900

    feat: 에러 페이지 추가 (mina-gwak/issue-tracker#8)

--- FE/src/pages/Error/index.tsx
+++ FE/src/pages/Error/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import Button from '@components/common/Button';
+import { BUTTON_SIZE } from '@components/common/Button/constants';
+import * as S from '@pages/Error/Error.style';
+
+interface LocationStateType {
+  message: string;
+}
 
 const Error = () => {
   const { state } = useLocation();
-  return <div></div>;
+  const message = (state as LocationStateType)?.message;
+
+  const navigate = useNavigate();
+
+  return (
+    <S.Wrapper>
+      <S.Title>Oops!</S.Title>
+      <S.BoldMessage>{ message }</S.BoldMessage>
+      <S.Message>잠시 후에 다시 시도해주세요.</S.Message>
+      <Button size={BUTTON_SIZE.MEDIUM} content={'처음으로 돌아가기'} onClick={() => navigate('/')} />
+    </S.Wrapper>
+  );
 };
 
 export default Error;
