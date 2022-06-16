2a891ae2d18f8fdf92ea955c3780c5229a137df4
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-06-16 10:52:59 +0900

    chore: styles 폴더 이름 변경  (mina-gwak/issue-tracker#6)

--- FE/src/index.tsx
+++ FE/src/index.tsx
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
-import GlobalStyle from '@assets/style/GlobalStyle';
-import { theme } from '@assets/style/theme';
+import GlobalStyle from '@assets/styles/GlobalStyle';
+import { theme } from '@assets/styles/theme';
 
 import App from './App';
 
