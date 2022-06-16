0b9daae781874ab75e7da4293af323454683e8e7
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-06-17 02:39:43 +0900

    feat: OAuth 로그인 구현  (mina-gwak/issue-tracker#8)

---
+++ FE/src/pages/LoginCallback/index.tsx
@@ -0,0 +1,52 @@
+import { useEffect } from 'react';
+
+import qs from 'qs';
+import { Navigate } from 'react-router-dom';
+
+import { API } from '@constants/api';
+import { ERROR } from '@constants/errorMessage';
+import useAxios from '@hooks/useAxios';
+import { useLocalStorage } from '@hooks/useLocalStorage';
+import Loading from '@pages/Loading';
+
+interface TokenResponseType {
+  tokenType: string;
+  accessToken: string;
+  refreshToken: string;
+}
+
+const LoginCallback = () => {
+  const [accessToken, setAccessToken] = useLocalStorage('accessToken', '');
+  const [_, setRefreshToken] = useLocalStorage('refreshToken', '');
+
+  const { code } = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+
+  const [{ response, error }] = useAxios<TokenResponseType>({
+    method: 'get',
+    url: API.GITHUB_LOGIN_CALLBACK,
+    config: {
+      params: {
+        code,
+      },
+    },
+  });
+
+  useEffect(() => {
+    if (response) {
+      setAccessToken(response.accessToken);
+      setRefreshToken(response.refreshToken);
+    }
+  }, [response]);
+
+  return error ? (
+    <Navigate to={'/error'} state={{ message: ERROR.LOGIN_ERROR }} replace />
+  ) : accessToken ? (
+    <Navigate to={'/issue-list'} replace />
+  ) : (
+    <Loading />
+  );
+};
+
+export default LoginCallback;
