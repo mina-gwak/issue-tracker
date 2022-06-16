9b685a6bb7974bef50cb3c72008429e7eb923e85
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-06-17 02:33:17 +0900

    design: 페이지 센터 정렬 추가  (mina-gwak/issue-tracker#8)

--- FE/src/assets/styles/mixin.ts
+++ FE/src/assets/styles/mixin.ts
@@ -4,4 +4,11 @@ export const flexCenter = css`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
+
+export const pageContentCenter = css`
+  ${flexCenter}
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+`;
