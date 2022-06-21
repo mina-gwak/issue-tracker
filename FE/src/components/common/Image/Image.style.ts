3b7fcbf67fcad7f905d9b83935329c0392741578
mina-gwak <mina.gwak@kakao.com>
2022-06-21 14:41:22 +0900

    refactor: 이미지 컴포넌트 변경 (mina-gwak/issue-tracker#13)
    - props 변경
    - 이미지 컴포넌트 스토리 추가

---
+++ FE/src/components/common/Image/Image.style.ts
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components';
+
+import { IMAGE_SIZE } from '@components/common/Image/constants';
+
+type StyledImageProps = {
+  size?: string;
+};
+
+export const sizeStyles = css<StyledImageProps>`
+  ${({ size }) =>
+    size === IMAGE_SIZE.SMALL &&
+    css`
+      display: block;
+      width: 1.25rem;
+      height: 1.25rem;
+      border: 1px solid ${({ theme }) => theme.colors.grey4};
+      border-radius: 50%;
+    `}
+
+  ${({ size }) =>
+    size === IMAGE_SIZE.LARGE &&
+    css`
+      display: block;
+      width: 2.75rem;
+      height: 2.75rem;
+      border: 1px solid ${({ theme }) => theme.colors.grey4};
+      border-radius: 50%;
+    `}
+`;
+
+export const Image = styled.img`
+  ${sizeStyles}
+`;
