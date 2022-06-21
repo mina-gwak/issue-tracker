28eb404b4746b48f1f614c21553db2b34ed57fad
mina-gwak <mina.gwak@kakao.com>
2022-06-21 14:46:43 +0900

    chore: 타입 디렉토리 경로 추가 (mina-gwak/issue-tracker#13)

---
+++ FE/src/type/issueType.ts
@@ -0,0 +1,28 @@
+export interface LabelType {
+  id: number;
+  name: string;
+  backgroundColor: string;
+  textColor: string;
+}
+
+export interface MilestoneType {
+  id: number;
+  title: string;
+}
+
+export interface MemberType {
+  id: number;
+  name: string;
+  imgUrl: string;
+}
+
+export interface IssueListType {
+  id: number;
+  num: number;
+  title: string;
+  labels: LabelType[];
+  milestone: MilestoneType;
+  assignees: MemberType[];
+  author: MemberType;
+  time: Date;
+}
