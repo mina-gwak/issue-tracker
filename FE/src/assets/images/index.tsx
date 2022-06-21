85b3487eec5ae83f3acc851b963a833c1f281459
mina-gwak <mina.gwak@kakao.com>
2022-06-21 14:43:56 +0900

    feat: 이미지 추가 (mina-gwak/issue-tracker#13)

--- FE/src/assets/images/index.tsx
+++ FE/src/assets/images/index.tsx
@@ -1,7 +1,7 @@
-import LargeLogo from '@assets/images/LargeLogo.webp';
+import LargeLogo from '@assets/images/largeLogo.svg';
+import MediumLogo from '@assets/images/mediumLogo.svg';
 
 export const Images = {
   LargeLogo,
+  MediumLogo,
 } as const;
-
-export type ImagesType = keyof typeof Images;
--- FE/src/components/common/Image/constants.ts
+++ FE/src/components/common/Image/constants.ts
@@ -1,3 +1,9 @@
 export const IMAGE_NAME = {
   LARGE_LOGO: 'LargeLogo',
+  MEDIUM_LOGO: 'MediumLogo',
+} as const;
+
+export const IMAGE_SIZE = {
+  SMALL: 'SMALL',
+  LARGE: 'LARGE',
 } as const;
