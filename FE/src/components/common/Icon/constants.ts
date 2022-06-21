feef1fee9e168ed7447d3fc57525fc13c86037db
mina-gwak <mina.gwak@kakao.com>
2022-06-21 14:38:17 +0900

    feat: 아이콘 추가 (mina-gwak/issue-tracker#13)

--- FE/src/components/common/Icon/constants.ts
+++ FE/src/components/common/Icon/constants.ts
@@ -1,5 +1,14 @@
 export const ICON_NAME = {
+  ARCHIVE: 'Archive',
+  ALERT_CIRCLE: 'AlertCircle',
+  CHECKBOX_ACTIVE: 'CheckBoxActive',
+  CHECKBOX_DISABLED: 'CheckBoxDisabled',
+  CHECKBOX_INITIAL: 'CheckBoxInitial',
   GITHUB: 'Github',
+  LABEL: 'Label',
+  MILESTONE: 'Milestone',
+  SEARCH: 'Search',
+  SELECT: 'Select',
 } as const;
 
 export const ICON_SIZE = {
