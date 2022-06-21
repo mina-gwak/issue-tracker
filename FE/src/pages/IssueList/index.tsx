2d6ba47c87b098d4260662f04df6f2fe056f21ea
mina-gwak <mina.gwak@kakao.com>
2022-06-21 15:04:15 +0900

    feat: 이슈 리스트 페이지 수정 (mina-gwak/issue-tracker#13)
    - 컴포넌트 조합하여 페이지 구성

--- FE/src/pages/IssueList/index.tsx
+++ FE/src/pages/IssueList/index.tsx
@@ -1,5 +1,16 @@
+import Header from '@components/Header';
+import IssueMenu from '@components/IssueMenu';
+import IssueTable from '@components/IssueTable';
+import * as S from '@pages/IssueList/IssueList.style';
+
 const IssueList = () => {
-  return <div>이슈 페이지</div>;
+  return (
+    <S.Wrapper>
+      <Header />
+      <IssueMenu />
+      <IssueTable />
+    </S.Wrapper>
+  );
 };
 
 export default IssueList;
