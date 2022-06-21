a048922b662c1299a4cd7c63367ded9be5e685bd
mina-gwak <mina.gwak@kakao.com>
2022-06-21 15:01:23 +0900

    feat: 이슈 테이블 컴포넌트 추가 (mina-gwak/issue-tracker#13)
    - 이슈 리스트 더미 데이터 추가

---
+++ FE/src/components/IssueTable/IssueTable.style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 1280px;
+  border: 1px solid ${({ theme }) => theme.colors.grey4};
+  border-radius: 11px;
+  overflow: hidden;
+`;
