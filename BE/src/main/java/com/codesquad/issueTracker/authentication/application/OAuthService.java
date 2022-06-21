5f289fb1823f51fc20760a3896c28d02d3cf5f9f
leejohy-0223 <leejohy@naver.com>
2022-06-21 23:45:49 +0900

    feat: 우선 더미데이터를 가져올 수 있도록 guest user 적용

--- BE/src/main/java/com/codesquad/issueTracker/authentication/application/OAuthService.java
+++ BE/src/main/java/com/codesquad/issueTracker/authentication/application/OAuthService.java
@@ -1,10 +1,13 @@
 package com.codesquad.issueTracker.authentication.application;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.codesquad.issueTracker.user.domain.UserRepository;
-import com.codesquad.issueTracker.authentication.domain.RedisTokenRepository;
+import com.codesquad.issueTracker.user.domain.User;
+import com.codesquad.issueTracker.user.domain.repository.UserRepository;
+import com.codesquad.issueTracker.authentication.domain.repository.RedisTokenRepository;
 import com.codesquad.issueTracker.authentication.infrastructure.JwtTokenProvider;
 import com.codesquad.issueTracker.authentication.infrastructure.OAuthClientServer;
 import com.codesquad.issueTracker.authentication.infrastructure.dto.OAuthTokenResponse;
@@ -33,15 +36,15 @@ public class OAuthService {
         OAuthTokenResponse accessToken = oAuthClientServer.getOAuthToken(code);
         UserProfileResponse userProfile = oAuthClientServer.getUserProfile(accessToken);
 
-        saveUser(userProfile);
+        User user = saveUser(userProfile);
+        String savedUserId = String.valueOf(user.getId());
 
-        String jwtAccessToken = jwtTokenProvider.generateAccessToken(userProfile.getName());
-        String jwtRefreshToken = jwtTokenProvider.generateRefreshToken(userProfile.getName());
-
+        String jwtAccessToken = jwtTokenProvider.generateAccessToken(savedUserId);
+        String jwtRefreshToken = jwtTokenProvider.generateRefreshToken(savedUserId);
         log.debug("jwtAccessToken : {}, refreshToken : {}", jwtAccessToken, jwtRefreshToken);
 
         // TODO : RollBack 적용 필요
-        redisTokenRepository.insert(userProfile.getName(), jwtRefreshToken);
+        redisTokenRepository.insert(savedUserId, jwtRefreshToken);
         return new OAuthLoginResponse("Bearer", jwtAccessToken, jwtRefreshToken, userProfile);
     }
 
@@ -49,14 +52,12 @@ public class OAuthService {
         redisTokenRepository.delete(username);
     }
 
-    private void saveUser(UserProfileResponse userProfile) {
-        if (userRepository.findByName(userProfile.getName()).isPresent()) {
-            return;
-        }
-        userRepository.save(userProfile.toEntity());
+    private User saveUser(UserProfileResponse userProfile) {
+        Optional<User> user = userRepository.findByName(userProfile.getName());
+        return user.orElseGet(() -> userRepository.save(userProfile.toEntity()));
     }
 
-    public boolean isLogout(String username) {
-        return redisTokenRepository.findByKey(username).isEmpty();
+    public boolean isLogout(String userId) {
+        return redisTokenRepository.findByKey(userId).isEmpty();
     }
 }
