fccad3cf5659ed0b365224f18c7243c22c097e99
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-06-22 20:46:04 +0900

    feat: 드롭다운 레이아웃 추가 (mina-gwak/issue-tracker#15)

--- FE/src/components/IssueMenu/FilterBar/index.tsx
+++ FE/src/components/IssueMenu/FilterBar/index.tsx
@@ -1,8 +1,15 @@
+import { useRecoilValue } from 'recoil';
+
 import * as S from '@components/IssueMenu/FilterBar/FilterBar.style';
 import Icon from '@components/common/Icon';
 import { ICON_NAME } from '@components/common/Icon/constants';
+import Modal from '@components/common/Modal';
+import { filter } from '@data/dropdownData';
+import { modalState } from '@store/atom/dropdown';
 
 const FilterBar = () => {
+  const modalValue: { [key: string]: boolean } = useRecoilValue(modalState);
+
   return (
     <S.Wrapper>
       <S.FilterButtonWrapper>
@@ -10,7 +17,7 @@ const FilterBar = () => {
           필터
           <Icon iconName={ICON_NAME.SELECT} />
         </S.FilterButton>
-        {/* 필터 드롭다운 */}
+        {modalValue['issue'] && <Modal data={filter} title='이슈' />}
       </S.FilterButtonWrapper>
       <S.FilterInputWrapper>
         <S.FilterInput type='text' placeholder='Search all issues' />
--- FE/src/components/IssueTable/TableHeader/Filter.tsx
+++ FE/src/components/IssueTable/TableHeader/Filter.tsx
@@ -1,15 +1,19 @@
+import { useRecoilValue } from 'recoil';
+
 import * as S from '@components/IssueTable/TableHeader/TableHeader.style';
 import Icon from '@components/common/Icon';
 import { ICON_NAME } from '@components/common/Icon/constants';
-
+import Modal from '@components/common/Modal';
+import { label } from '@data/dropdownData';
+import { modalState } from '@store/atom/dropdown';
 interface FilterPropsType {
   type: string;
   title: string;
 }
 
 const Filter = ({ type, title }: FilterPropsType) => {
-
   // TODO: type 값을 활용한 data fetching
+  const modalValue: { [key: string]: boolean } = useRecoilValue(modalState);
 
   return (
     <>
@@ -17,9 +21,9 @@ const Filter = ({ type, title }: FilterPropsType) => {
         {title}
         <Icon iconName={ICON_NAME.SELECT} />
       </S.FilterButton>
-      {/* 필터 드롭다운 */}
+      {modalValue[type] && <Modal data={label} title={title} position='right' />}
     </>
-  )
-}
+  );
+};
 
 export default Filter;
