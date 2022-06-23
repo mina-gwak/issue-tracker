83321eb240763ed0cdd4dd4afb5930a8c66fa97d
mina-gwak <mina.gwak@kakao.com>
2022-06-23 14:17:02 +0900

    feat: MSW 적용 (mina-gwak/issue-tracker#16)

--- FE/src/components/IssueTable/index.tsx
+++ FE/src/components/IssueTable/index.tsx
@@ -1,10 +1,20 @@
 import Issue from '@components/IssueTable/Issue';
 import * as S from '@components/IssueTable/IssueTable.style';
 import TableHeader from '@components/IssueTable/TableHeader';
+import { API } from '@constants';
 import { issueDummyData } from '@data';
+import useAxios from '@hooks/useAxios';
 import { IssueListType } from '@type/issueType';
 
 const IssueTable = () => {
+
+  const [{ response, isLoading, error }] = useAxios<IssueListType[]>({
+    method: 'get',
+    url: API.ISSUE,
+  });
+
+  console.log(isLoading, response, error);
+
   return (
     <S.Wrapper>
       <TableHeader />
---
+++ FE/src/constants/index.ts
@@ -0,0 +1,3 @@
+export { ERROR } from '@constants/errorMessage';
+export { API } from '@constants/api';
+export { BASE_URL } from '@constants/baseUrl';
