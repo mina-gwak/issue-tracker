b9f320d05a056c197bc2161549fc9c1d5e0c4184
mina-gwak <mina.gwak@kakao.com>
2022-06-28 20:45:04 +0900

    refactor: 체크박스 구조 변경 (mina-gwak/issue-tracker#16)

--- FE/src/components/common/CheckBox/index.tsx
+++ FE/src/components/common/CheckBox/index.tsx
@@ -1,17 +1,17 @@
-import { useMemo } from 'react';
+import { useId, useMemo } from 'react';
 
 import * as S from '@components/common/CheckBox/CheckBox.style';
 import Icon from '@components/common/Icon';
 import { ICON_NAME } from '@components/common/Icon/constants';
 
 export interface CheckBoxPropsType {
-  checkBoxId: string;
   isChecked: boolean;
   isAllChecked?: boolean;
   toggleIsChecked: () => void;
 }
 
-const CheckBox = ({ checkBoxId, isChecked, toggleIsChecked, isAllChecked = true }: CheckBoxPropsType) => {
+const CheckBox = ({ isChecked, toggleIsChecked, isAllChecked = true }: CheckBoxPropsType) => {
+  const checkBoxId = useId();
 
   const checkBoxIcon = useMemo(() => {
     if (isChecked && isAllChecked) return ICON_NAME.CHECKBOX_ACTIVE;
@@ -26,7 +26,7 @@ const CheckBox = ({ checkBoxId, isChecked, toggleIsChecked, isAllChecked = true 
       </S.Label>
       <S.CheckBoxInput type='checkbox' id={checkBoxId} />
     </>
-  )
-}
+  );
+};
 
 export default CheckBox;
