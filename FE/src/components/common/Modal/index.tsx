03105686f2443e2a345ff85fc69dd2bb06836d07
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-06-28 17:07:40 +0900

    feat:  모달창 스타일 변경 및 아이콘 추가(mina-gwak/issue-tracker#15)

--- FE/src/components/common/Modal/index.tsx
+++ FE/src/components/common/Modal/index.tsx
@@ -1,6 +1,10 @@
+import Icon from '@components/common/Icon';
+import { ICON_NAME } from '@components/common/Icon/constants';
+import Image from '@components/common/Image';
 import * as S from '@components/common/Modal/Modal.style';
 import { DropdownType } from '@type/dropdownType';
 
+import { IMAGE_SIZE } from '../Image/constants';
 export interface ModalPropsType {
   data: DropdownType[];
   title: string;
@@ -8,13 +12,25 @@ export interface ModalPropsType {
 }
 
 const Modal = ({ data, title, position = 'left' }: ModalPropsType) => {
+  const CheckBoxIcon = true ? (
+    <Icon iconName={ICON_NAME.CHECKBOX_CIRCLE_ACTIVE} />
+  ) : (
+    <Icon iconName={ICON_NAME.CHECKBOX_CIRCLE_INITIAL} />
+  );
+
   return (
     <S.MenuList position={position}>
       <S.MenuTitle>{title} 필터</S.MenuTitle>
-      {data.map(({ id, optionName }) => (
+      {data.map(({ id, optionName, circleIcon, colorIcon }) => (
         <S.MenuOptionGroup key={id}>
-          <S.MenuItemOption>{optionName}</S.MenuItemOption>
-          <span>체크</span>
+          <S.MenuOptionItem>
+            {circleIcon && <Image url={circleIcon} alt={optionName} size={IMAGE_SIZE.SMALL} />}
+
+            {colorIcon && <S.CircleColorIcon colorIcon={colorIcon} />}
+
+            <S.MenuItemOption>{optionName}</S.MenuItemOption>
+          </S.MenuOptionItem>
+          {CheckBoxIcon}
         </S.MenuOptionGroup>
       ))}
     </S.MenuList>
