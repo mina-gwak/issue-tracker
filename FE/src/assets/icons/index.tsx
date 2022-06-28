f0080ea007f6a3db09511a55e146ed7993d731f9
mina-gwak <mina.gwak@kakao.com>
2022-06-29 02:33:21 +0900

    refactor: SVG 사용 방식 변경, Icon 컴포넌트 수정 (mina-gwak/issue-tracker#28)

--- FE/src/assets/icons/index.tsx
+++ FE/src/assets/icons/index.tsx
@@ -1,15 +1,15 @@
-import { ReactComponent as AlertCircle } from '@assets/icons/alertCircle.svg';
-import { ReactComponent as Archive } from '@assets/icons/archive.svg';
-import { ReactComponent as CheckBoxActive } from '@assets/icons/checkBoxActive.svg';
-import { ReactComponent as CheckBoxCircleActive } from '@assets/icons/checkBoxCircleActive.svg';
-import { ReactComponent as CheckBoxCircleInitial } from '@assets/icons/checkBoxCircleInitial.svg';
-import { ReactComponent as CheckBoxDisabled } from '@assets/icons/checkBoxDisabled.svg';
-import { ReactComponent as CheckBoxInitial } from '@assets/icons/checkBoxInitial.svg';
-import { ReactComponent as Github } from '@assets/icons/github.svg';
-import { ReactComponent as Label } from '@assets/icons/label.svg';
-import { ReactComponent as Milestone } from '@assets/icons/milestone.svg';
-import { ReactComponent as Search } from '@assets/icons/search.svg';
-import { ReactComponent as Select } from '@assets/icons/select.svg';
+import AlertCircle from '@assets/icons/alertCircle.svg';
+import Archive from '@assets/icons/archive.svg';
+import CheckBoxActive from '@assets/icons/checkBoxActive.svg';
+import CheckBoxCircleActive from '@assets/icons/checkBoxCircleActive.svg';
+import CheckBoxCircleInitial from '@assets/icons/checkBoxCircleInitial.svg';
+import CheckBoxDisabled from '@assets/icons/checkBoxDisabled.svg';
+import CheckBoxInitial from '@assets/icons/checkBoxInitial.svg';
+import Github from '@assets/icons/github.svg';
+import Label from '@assets/icons/label.svg';
+import Milestone from '@assets/icons/milestone.svg';
+import Search from '@assets/icons/search.svg';
+import Select from '@assets/icons/select.svg';
 
 export const Icons = {
   Archive,
