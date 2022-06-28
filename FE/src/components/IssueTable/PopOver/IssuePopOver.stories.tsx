a8d41310b23ffcfc799609136cd8e383e939d5fb
mina-gwak <mina.gwak@kakao.com>
2022-06-28 20:55:55 +0900

    feat: useIssuePopOver 쿼리 적용 (mina-gwak/issue-tracker#16)

--- FE/src/components/IssueTable/PopOver/IssuePopOver.stories.tsx
+++ FE/src/components/IssueTable/PopOver/IssuePopOver.stories.tsx
@@ -1,29 +1,24 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { BrowserRouter } from 'react-router-dom';
 
-import IssuePopOver from '@components/IssueTable/PopOver/IssuePopOver';
+import IssuePopOver, { IssuePopOverPropsType } from '@components/IssueTable/PopOver/IssuePopOver';
 import * as S from '@components/common/PopOver/PopOver.style';
-import { issueDummyData } from '@data';
-import { IssueListType } from '@type/issueType';
 
 export default {
   title: 'Component/IssueTable/IssuePopOver',
   component: IssuePopOver,
   args: {
-    ...issueDummyData[1],
+    issueId: 2,
   }
 } as ComponentMeta<typeof IssuePopOver>;
 
-export const Default: ComponentStory<typeof IssuePopOver> = (args: IssueListType) => {
+export const Default: ComponentStory<typeof IssuePopOver> = (args: IssuePopOverPropsType) => {
   const element = document.getElementById('root') as HTMLElement;
   element.style.position = 'relative';
 
   return (
-    <BrowserRouter>
-      <S.StoryWrapper>
-        Element
-        <IssuePopOver {...args} />
-      </S.StoryWrapper>
-    </BrowserRouter>
+    <S.StoryWrapper>
+      Element
+      <IssuePopOver {...args} />
+    </S.StoryWrapper>
   );
 };
