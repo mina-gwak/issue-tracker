235f00ffa82e24e4c5c1eeb80b53f38e15696a8a
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-06-29 16:06:20 +0900

    feat: filterBarValue 변경 (mina-gwak/issue-tracker#15)

--- FE/src/components/IssueMenu/FilterBar/index.tsx
+++ FE/src/components/IssueMenu/FilterBar/index.tsx
@@ -6,11 +6,12 @@ import Icon from '@components/common/Icon';
 import { ICON_NAME } from '@components/common/Icon/constants';
 import { filter } from '@data/dropdownData';
 import { useModal } from '@hooks/useModal';
-import { modalState } from '@store/atom/dropdown';
+import { modalState } from '@store/dropdown';
+import { filterBarInputValueState } from '@store/filterBar';
 
 const FilterBar = () => {
   const modalValue: { [key: string]: boolean } = useRecoilValue(modalState);
-
+  const filterBarValue = useRecoilValue(filterBarInputValueState);
   const { toggleModal } = useModal();
 
   return (
@@ -23,7 +24,7 @@ const FilterBar = () => {
         {modalValue['issue'] && <Modal data={filter} title='이슈' />}
       </S.FilterButtonWrapper>
       <S.FilterInputWrapper>
-        <S.FilterInput type='text' placeholder='Search all issues' />
+        <S.FilterInput type='text' placeholder='Search all issues' value={filterBarValue} />
         <Icon iconName={ICON_NAME.SEARCH} />
       </S.FilterInputWrapper>
     </S.Wrapper>
