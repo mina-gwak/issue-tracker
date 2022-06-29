5cbdc612b0a483b4f777de78cb72b478a63e4199
leejohy-0223 <leejohy@naver.com>
2022-06-29 13:32:48 +0900

    feat: image Entity 추가

---
+++ BE/src/main/java/com/codesquad/issueTracker/issue/domain/Image.java
@@ -0,0 +1,34 @@
+package com.codesquad.issueTracker.issue.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "issue_id")
+    private Issue issue;
+
+    public Image(String imageUrl, Issue issue) {
+        this.imageUrl = imageUrl;
+        this.issue = issue;
+    }
+}
