9bd3671306c4bdad226d726e061b4960d315fe3d
mina-gwak <mina.gwak@kakao.com>
2022-06-30 17:51:40 +0900

    feat: useCheckBox 훅 생성 및 적용 (mina-gwak/issue-tracker#32)

--- FE/src/components/IssueTable/TableHeader/index.tsx
+++ FE/src/components/IssueTable/TableHeader/index.tsx
@@ -1,46 +1,64 @@
-import { useState } from 'react';
-
 import Filter from '@components/IssueTable/TableHeader/Filter';
 import * as S from '@components/IssueTable/TableHeader/TableHeader.style';
 import CheckBox from '@components/common/CheckBox';
 import Icon from '@components/common/Icon';
 import { ICON_NAME } from '@components/common/Icon/constants';
 import { filterList } from '@data';
+import useCheckBox from '@hooks/useCheckBox';
 
 export interface TableHeaderPropsType {
   count: [number, number];
 }
 
 const TableHeader = ({ count: [openedIssue, closedIssue] }: TableHeaderPropsType) => {
-  const [isChecked, setIsChecked] = useState(false);
-  const [isAllChecked, setIsAllChecked] = useState(false);
-
-  const toggleIsChecked = () => setIsChecked((prevState) => !prevState);
+  const { isCheckedItems, isAllChecked, toggleIsAllChecked } = useCheckBox();
+  const isChecked = isCheckedItems.size > 0;
 
   return (
     <S.Wrapper>
-      <CheckBox isChecked={isChecked} toggleIsChecked={toggleIsChecked} isAllChecked={isAllChecked} />
-      <S.IssueTabs>
-        <li>
-          <S.IssueTab>
-            <Icon iconName={ICON_NAME.ALERT_CIRCLE} />
-            열린 이슈({openedIssue})
-          </S.IssueTab>
-        </li>
-        <li>
-          <S.IssueTab>
-            <Icon iconName={ICON_NAME.ARCHIVE} />
-            닫힌 이슈({closedIssue})
-          </S.IssueTab>
-        </li>
-      </S.IssueTabs>
-      <S.FilterList>
-        {filterList.map(({ id, ...props }) => (
-          <S.FilterListItem key={id}>
-            <Filter {...props} />
-          </S.FilterListItem>
-        ))}
-      </S.FilterList>
+      <CheckBox
+        isChecked={isChecked}
+        toggleIsChecked={toggleIsAllChecked}
+        isAllChecked={isAllChecked}
+      />
+      {isChecked ? (
+        <>
+          <S.IssueTabs>
+            <li>
+              <S.IssueTab>{isCheckedItems.size}개 이슈 선택</S.IssueTab>
+            </li>
+          </S.IssueTabs>
+          <S.FilterList>
+            <S.FilterListItem>
+              <Filter type='stateModify' title='상태 수정' />
+            </S.FilterListItem>
+          </S.FilterList>
+        </>
+      ) : (
+        <>
+          <S.IssueTabs>
+            <li>
+              <S.IssueTab>
+                <Icon iconName={ICON_NAME.ALERT_CIRCLE} />
+                열린 이슈({openedIssue})
+              </S.IssueTab>
+            </li>
+            <li>
+              <S.IssueTab>
+                <Icon iconName={ICON_NAME.ARCHIVE} />
+                닫힌 이슈({closedIssue})
+              </S.IssueTab>
+            </li>
+          </S.IssueTabs>
+          <S.FilterList>
+            {filterList.map(({ id, ...props }) => (
+              <S.FilterListItem key={id}>
+                <Filter {...props} />
+              </S.FilterListItem>
+            ))}
+          </S.FilterList>
+        </>
+      )}
     </S.Wrapper>
   );
 };
