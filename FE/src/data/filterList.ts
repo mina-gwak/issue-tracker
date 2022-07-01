b723be89c28377cb1fd6715a2c22cac9477b12bc
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-07-01 11:09:25 +0900

    feat: 목 데이터 변경 (mina-gwak/issue-tracker#31)

--- FE/src/data/filterList.ts
+++ FE/src/data/filterList.ts
@@ -2,21 +2,21 @@ export const filterList = [
   {
     id: 1,
     type: 'author',
-    title: '담당자'
+    title: '작성자',
   },
   {
     id: 2,
     type: 'label',
-    title: '레이블'
+    title: '레이블',
   },
   {
     id: 3,
     type: 'milestone',
-    title: '마일스톤'
+    title: '마일스톤',
   },
   {
     id: 4,
     type: 'assignee',
-    title: '작성자'
+    title: '담당자',
   },
 ] as const;
