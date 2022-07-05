4bf705d1f560b28bfbf52e2f4bdcb15b99af3f71
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-07-05 13:57:22 +0900

    feat: FILTER API 추가 (mina-gwak/issue-tracker#34)

--- FE/src/constants/api.ts
+++ FE/src/constants/api.ts
@@ -2,4 +2,5 @@ export const API = {
   GITHUB_LOGIN_CALLBACK: '/api/oauth/login/github/callback',
   ISSUE: '/api/issues',
   ISSUE_POPOVER: (issueId: number) => `/api/issues/${issueId}/popUp`,
+  FILTER: (type: string) => `/api/${type}`,
 } as const;
