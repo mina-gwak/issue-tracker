01250d435d107e135d6a8a74ba20c2f1b688a5b5
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-07-06 14:19:32 +0900

    feat: 데이터 패치 추가 (mina-gwak/issue-tracker#34)

--- FE/src/components/IssueTable/TableHeader/Filter.tsx
+++ FE/src/components/IssueTable/TableHeader/Filter.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+
+import axios from 'axios';
 import { useRecoilValue } from 'recoil';
 
 import * as S from '@components/IssueTable/TableHeader/TableHeader.style';
@@ -5,6 +8,7 @@ import Modal from '@components/Modal';
 import { POSITION } from '@components/Modal/constants';
 import Icon from '@components/common/Icon';
 import { ICON_NAME } from '@components/common/Icon/constants';
+import { API } from '@constants';
 import { labels, milestones, assignees, writers } from '@data/dropdownData';
 import { useModal } from '@hooks/useModal';
 import { modalState, ModalStateType } from '@store/dropdown';
@@ -23,10 +27,24 @@ const fetchData: { [key: string]: DropdownType[] } = {
 };
 
 const Filter = ({ type, title }: FilterPropsType) => {
+  const [filterValue, setFilterValue] = useState();
   const modalValue = useRecoilValue(modalState);
   const { toggleModal } = useModal();
   const curData = fetchData[type];
 
+  const fetchTypeData = async () => {
+    if (filterValue) return;
+    const accessToken = localStorage.getItem('accessToken')!;
+
+    const response = await axios.get(`${API.FILTER(type)}`, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(accessToken)}`,
+      },
+    });
+
+    setFilterValue(response.data[`${type}OutlineResponses`]);
+  };
+  // onMouseEnter={fetchTypeData} 50번째 줄 추카
   return (
     <>
       <S.FilterButton onClick={toggleModal(type)}>
