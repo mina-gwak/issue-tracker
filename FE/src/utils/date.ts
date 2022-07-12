404cc1db46dd8872fe2151fb9a90eaacba3722d2
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-07-13 03:01:29 +0900

    chore: 세미콜론 추가 (mina-gwak/issue-tracker#40)

--- FE/src/utils/date.ts
+++ FE/src/utils/date.ts
@@ -6,11 +6,11 @@ export const calcTwoTimeDifference = (currentDate: Date, updatedDate: string) =>
   if (result < 60) return `${result.toFixed()}분전`;
   else if (result < 1440) return `${(result / 60).toFixed()}시간 ${(result % 60).toFixed()}분전`;
   return `${(result / 1440).toFixed()}일 전`;
-}
+};
 
 const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 export const getDate = (registeredDate: string) => {
   const date = new Date(registeredDate);
   return `${month[date.getMonth()]} ${date.getDate()}`;
-}
+};
