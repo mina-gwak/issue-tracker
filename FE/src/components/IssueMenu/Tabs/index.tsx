dcfba8e8ef4cc6835f5e032e1715100c1a830370
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-07-13 02:56:39 +0900

    feat: 탭 클릭시 페이지 이동 (mina-gwak/issue-tracker#40)

--- FE/src/components/IssueMenu/Tabs/index.tsx
+++ FE/src/components/IssueMenu/Tabs/index.tsx
@@ -1,19 +1,28 @@
+import { useRecoilValue } from 'recoil';
+
 import * as S from '@components/IssueMenu/Tabs/Tabs.style';
 import Icon from '@components/common/Icon';
 import { ICON_NAME } from '@components/common/Icon/constants';
+import { useIssuesQuery } from '@query/issue';
+import { filterBarQueryString } from '@store/filterBar';
+import { IssueDataType } from '@type/issueType';
 
 const Tabs = () => {
+  const filterBarValue = useRecoilValue(filterBarQueryString);
+
+  const { data } = useIssuesQuery<IssueDataType>(filterBarValue);
+
   return (
     <S.TabContainer>
-      <S.Tab to='/레이블'>
+      <S.Tab to='../label'>
         <Icon iconName={ICON_NAME.LABEL} />
         <S.TabTitle>레이블</S.TabTitle>
-        <span>({3})</span>
+        <span>({data?.labelCount && data?.labelCount})</span>
       </S.Tab>
-      <S.Tab to='/마일스톤'>
+      <S.Tab to='../milestone'>
         <Icon iconName={ICON_NAME.MILESTONE} />
         <S.TabTitle>마일스톤</S.TabTitle>
-        <span>({2})</span>
+        <span>({data?.milestoneCount && data?.milestoneCount})</span>
       </S.Tab>
     </S.TabContainer>
   );
