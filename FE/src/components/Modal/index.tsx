9192f7ebc912cd83372ab24d378fb6df4ff96486
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-07-13 03:05:24 +0900

    feat : 상태변경 모달 클릭시 fetch 함수 추가 (mina-gwak/issue-tracker#40)

--- FE/src/components/Modal/index.tsx
+++ FE/src/components/Modal/index.tsx
@@ -1,5 +1,7 @@
 import { RefObject, useEffect } from 'react';
 
+import axios from 'axios';
+import qs from 'qs';
 import { useRecoilValue, useRecoilState } from 'recoil';
 
 import { IconsType } from '@assets/icons';
@@ -9,16 +11,24 @@ import Icon from '@components/common/Icon';
 import { ICON_NAME } from '@components/common/Icon/constants';
 import Image from '@components/common/Image';
 import { IMAGE_SIZE } from '@components/common/Image/constants';
+import { API } from '@constants';
+import useCheckBox from '@hooks/useCheckBox';
 import { filterBarArrState, filterBarState } from '@store/filterBar';
 import { DropdownType } from '@type/dropdownType';
+import { queryClient } from '@utils/queryClient';
 
 export interface ModalPropsType {
   modalRef: RefObject<HTMLDivElement>;
-  data: DropdownType[];
+  data?: DropdownType[];
   title: string;
   type: string;
   position?: string;
   handleModalClick: (event: MouseEvent) => void;
+}
+
+interface CheckedListType {
+  id: number[];
+  status: string;
 }
 
 type isCheckedType = {
@@ -39,6 +49,7 @@ const Modal = ({
 }: ModalPropsType) => {
   const filterBarArrValue = useRecoilValue(filterBarArrState);
   const [filterBarValue, setFilterBarValue] = useRecoilState(filterBarState);
+  const { isCheckedItems, resetCheckedItems } = useCheckBox();
 
   const isChecked: isCheckedType = (value) => {
     let checkBoxIcon: IconsType = ICON_NAME.CHECKBOX_CIRCLE_INITIAL;
@@ -59,11 +70,13 @@ const Modal = ({
 
   const checkBoxClickHandler: checkBoxClickHandlerType = (value) => () => {
     if (!value) return;
-    for (const [objKey, objValue] of filterBarArrValue) {
+    for (const [, objValue] of filterBarArrValue) {
       if (Array.isArray(objValue)) {
         const checkSameValue = objValue.includes(value);
         if (type === 'is') {
           setFilterBarValue({ ...filterBarValue, [type]: ['issue', value] });
+        } else if (type === 'stateModify') {
+          fetchStateModify(value);
         } else {
           if (!checkSameValue) {
             setFilterBarValue({ ...filterBarValue, [type]: [value] });
@@ -74,6 +87,26 @@ const Modal = ({
         }
       }
     }
+  };
+
+  const fetchStateModify = async (value: string) => {
+    const accessToken = localStorage.getItem('accessToken')!;
+    const checkedList = { id: Array.from(isCheckedItems), status: value };
+
+    const myAxios = axios.create({
+      paramsSerializer: (checkedList: CheckedListType) =>
+        qs.stringify(checkedList, { arrayFormat: 'repeat' }),
+    });
+
+    await myAxios.post(`${API.STATUS_UPDATE()}`, null, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(accessToken)}`,
+      },
+      params: checkedList,
+    });
+
+    queryClient.invalidateQueries('issues');
+    resetCheckedItems();
   };
 
   useEffect(() => {
@@ -87,18 +120,19 @@ const Modal = ({
   return (
     <S.MenuList position={position} ref={modalRef}>
       <S.MenuTitle>{title} 필터</S.MenuTitle>
-      {data.map(({ optionName, value, imageUrl, colorCode }) => (
-        <S.MenuOptionGroup key={optionName}>
-          <S.MenuOptionItem>
-            {imageUrl && <Image url={imageUrl} alt={optionName} size={IMAGE_SIZE.SMALL} />}
-            {colorCode && <S.CircleColorIcon colorIcon={colorCode} />}
-            <S.MenuItemOption>{optionName}</S.MenuItemOption>
-          </S.MenuOptionItem>
-          <button onClick={checkBoxClickHandler(value || optionName)}>
-            <Icon iconName={isChecked(value || optionName)} />
-          </button>
-        </S.MenuOptionGroup>
-      ))}
+      {data &&
+        data.map(({ optionName, value, imageUrl, colorCode }) => (
+          <S.MenuOptionGroup key={optionName}>
+            <S.MenuOptionItem>
+              {imageUrl && <Image url={imageUrl} alt={optionName} size={IMAGE_SIZE.SMALL} />}
+              {colorCode && <S.CircleColorIcon colorIcon={colorCode} />}
+              <S.MenuItemOption>{optionName}</S.MenuItemOption>
+            </S.MenuOptionItem>
+            <button onClick={checkBoxClickHandler(value || optionName)}>
+              <Icon iconName={isChecked(value || optionName)} />
+            </button>
+          </S.MenuOptionGroup>
+        ))}
     </S.MenuList>
   );
 };
