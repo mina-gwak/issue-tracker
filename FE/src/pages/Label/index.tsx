98a268785c9f356f43c3f35028a3de4e63f2b9a2
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-07-13 02:55:21 +0900

    feat: 라벨 & 마일스톤 페이지 추가 (mina-gwak/issue-tracker#40)

---
+++ FE/src/pages/Label/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Label = () => {
+  return <div>라벨 페이지</div>;
+};
+
+export default Label;
---
+++ FE/src/pages/Milestone/index.tsx
@@ -0,0 +1,5 @@
+const Milestone = () => {
+  return <div>마일스톤 페이지</div>;
+};
+
+export default Milestone;
