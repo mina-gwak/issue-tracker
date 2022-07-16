828224f3cca0ff84a29ac6ad53ab0f6ae5b92580
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-07-16 19:21:28 +0900

    feat : 페이지 헤더 위치 변경 (mina-gwak/issue-tracker#50)

--- FE/src/pages/IssueList/index.tsx
+++ FE/src/pages/IssueList/index.tsx
@@ -1,4 +1,3 @@
-import Header from '@components/Header';
 import IssueMenu from '@components/IssueMenu';
 import IssueTable from '@components/IssueTable';
 import * as S from '@pages/IssueList/IssueList.style';
@@ -6,7 +5,6 @@ import * as S from '@pages/IssueList/IssueList.style';
 const IssueList = () => {
   return (
     <S.Wrapper>
-      <Header />
       <IssueMenu />
       <IssueTable />
     </S.Wrapper>
