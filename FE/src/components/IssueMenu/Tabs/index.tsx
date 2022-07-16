ced41409afd20810ebd090fd0c527d729764ddfd
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-07-16 19:30:38 +0900

    feat : 탭 액티브 효과 (mina-gwak/issue-tracker#50)

--- FE/src/components/IssueMenu/Tabs/index.tsx
+++ FE/src/components/IssueMenu/Tabs/index.tsx
@@ -5,21 +5,22 @@ import Icon from '@components/common/Icon';
 import { ICON_NAME } from '@components/common/Icon/constants';
 import { useIssuesQuery } from '@query/issue';
 import { filterBarQueryString } from '@store/filterBar';
+import { labelMilestoneActiveState } from '@store/labelMilestoneActive';
 import { IssueDataType } from '@type/issueType';
 
 const Tabs = () => {
   const filterBarValue = useRecoilValue(filterBarQueryString);
-
+  const labelMilestoneActiveValue = useRecoilValue(labelMilestoneActiveState);
   const { data } = useIssuesQuery<IssueDataType>(filterBarValue);
 
   return (
     <S.TabContainer>
-      <S.Tab to='../label'>
+      <S.Tab to='../label' active={labelMilestoneActiveValue.label.toString()}>
         <Icon iconName={ICON_NAME.LABEL} />
         <S.TabTitle>레이블</S.TabTitle>
         <span>({data?.labelCount && data?.labelCount})</span>
       </S.Tab>
-      <S.Tab to='../milestone'>
+      <S.Tab to='../milestone' active={labelMilestoneActiveValue.milestone.toString()}>
         <Icon iconName={ICON_NAME.MILESTONE} />
         <S.TabTitle>마일스톤</S.TabTitle>
         <span>({data?.milestoneCount && data?.milestoneCount})</span>
