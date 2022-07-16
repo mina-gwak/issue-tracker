e33fed989bdaf6bfbb870f64874f4e2a326e95f8
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-07-16 19:33:16 +0900

    feat : 라벨 페이지 첫 화면 (mina-gwak/issue-tracker#50)

--- FE/src/pages/Label/index.tsx
+++ FE/src/pages/Label/index.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { useSetRecoilState } from 'recoil';
+import styled from 'styled-components';
+
+import LabelCreateEditForm from '@components/LabelCreateEditForm';
+import LabelPageHeader from '@components/LabelPageHeader';
+import LabelTable from '@components/LabelTable';
+import { labelMilestoneActiveState } from '@store/labelMilestoneActive';
 
 const Label = () => {
-  return <div>라벨 페이지</div>;
+  const [isCreateLabel, setIsCreateLabel] = useState(false);
+  const setLabelMilestoneActiveValue = useSetRecoilState(labelMilestoneActiveState);
+
+  const handelClickCancel = () => setIsCreateLabel(false);
+  useEffect(() => {
+    setLabelMilestoneActiveValue({ label: true, milestone: false });
+  }, []);
+
+  return (
+    <Wrapper>
+      <LabelPageHeader isCreateLabel={isCreateLabel} setIsCreateLabel={setIsCreateLabel} />
+      {isCreateLabel && (
+        <LabelCreateEditForm title='새로운 레이블 추가' handelClickCancel={handelClickCancel} />
+      )}
+      <LabelTable />
+    </Wrapper>
+  );
 };
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1440px;
+  padding: 0 80px;
+  margin: 0 auto 120px;
+`;
 export default Label;
