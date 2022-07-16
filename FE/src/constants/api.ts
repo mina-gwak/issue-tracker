edf4ba9d94e55a9c6bd0ad541a84a6d73d953ea9
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-07-16 19:35:58 +0900

    feat : 라벨 페이지 테이블  CRUD (mina-gwak/issue-tracker#50)

--- FE/src/constants/api.ts
+++ FE/src/constants/api.ts
@@ -3,5 +3,9 @@ export const API = {
   ISSUE: '/api/issues',
   ISSUE_POPOVER: (issueId: number) => `/api/issues/${issueId}/popUp`,
   FILTER: (type: string) => `/api/${type}`,
-  STATUS_UPDATE: () => ` /api/issues/status`,
+  STATUS_UPDATE: () => `/api/issues/status`,
+  LABEL: () => `/api/labels/list`,
+  LABEL_CREATE: () => `/api/labels/`,
+  LABEL_UPDATE: (id: number) => `/api/labels/${id}`,
+  LABEL_DELETE: (id: number) => `/api/labels/${id}`,
 } as const;
---
+++ FE/src/utils/api/fetchLabel.ts
@@ -0,0 +1,75 @@
+import axios from 'axios';
+
+import { API } from '@constants';
+import { LabelCreateType, LabelDataType } from '@type/label';
+
+export const getLabelsData = async (): Promise<LabelDataType> => {
+  const accessToken = localStorage.getItem('accessToken')!;
+  try {
+    const response = await axios.get(`${API.LABEL()}`, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(accessToken)}`,
+      },
+    });
+
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const createLabel = async (newLabelData: LabelCreateType) => {
+  const accessToken = localStorage.getItem('accessToken')!;
+  console.log(newLabelData);
+  try {
+    const response = await axios.post(
+      `${API.LABEL_CREATE()}`,
+      { ...newLabelData },
+      {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(accessToken)}`,
+        },
+      },
+    );
+    if (response.status === 200) return true;
+    else throw Error('잘못된 생성입니다.');
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const editLabel = async (id: number, newLabelData: LabelCreateType) => {
+  const accessToken = localStorage.getItem('accessToken')!;
+
+  try {
+    const response = await axios.patch(
+      `${API.LABEL_UPDATE(id)}`,
+      { ...newLabelData },
+      {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(accessToken)}`,
+        },
+      },
+    );
+    if (response.status === 200) return true;
+    else throw Error('잘못된 생성입니다.');
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteLabel = async (id: number) => {
+  const accessToken = localStorage.getItem('accessToken')!;
+  try {
+    const response = await axios.delete(`${API.LABEL_DELETE(id)}`, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(accessToken)}`,
+      },
+    });
+
+    if (response.status === 200) return true;
+    else throw Error('잘못된 생성입니다.');
+  } catch (err) {
+    throw err;
+  }
+};
