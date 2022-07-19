477247e9cefbdefb6d544e8cd89e404eb98927f6
mina-gwak <mina.gwak@kakao.com>
2022-07-20 04:56:20 +0900

    feat: 헤더 컴포넌트 위치 변경 (mina-gwak/issue-tracker#42)

--- FE/src/router/ProtectedRoute.tsx
+++ FE/src/router/ProtectedRoute.tsx
@@ -1,11 +1,19 @@
 import { Navigate, Outlet } from 'react-router-dom';
 
+import Header from '@components/Header';
 import { useLocalStorage } from '@hooks/useLocalStorage';
 
 const ProtectedRoute = () => {
   const [accessToken] = useLocalStorage('accessToken', '');
 
-  return accessToken ? <Outlet /> : <Navigate to={'login'} replace />;
+  return accessToken ? (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  ) : (
+    <Navigate to={'login'} replace />
+  );
 };
 
 export default ProtectedRoute;
