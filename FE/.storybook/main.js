08692d35fa6e45451fef919d8298a7fd6f8decb2
mina-gwak <mina.gwak@kakao.com>
2022-07-20 04:57:09 +0900

    chore: src 경로 설정 (mina-gwak/issue-tracker#42)

--- FE/.storybook/main.js
+++ FE/.storybook/main.js
@@ -17,6 +17,7 @@ module.exports = {
     config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules', 'styles'];
     config.resolve.alias = {
       ...config.resolve.alias,
+      '@src': path.resolve(__dirname, '../src'),
       '@api': path.resolve(__dirname, '../src/api'),
       '@assets': path.resolve(__dirname, '../src/assets'),
       '@context': path.resolve(__dirname, '../src/context'),
