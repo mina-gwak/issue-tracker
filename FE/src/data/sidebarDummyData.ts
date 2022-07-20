2865a8113d58f620b9ee59e8033639dbbbd3162d
mina-gwak <mina.gwak@kakao.com>
2022-07-21 05:04:43 +0900

    feat: SideBar 임시 데이터 추가 (mina-gwak/issue-tracker#57)

---
+++ FE/src/data/sidebarDummyData.ts
@@ -0,0 +1,28 @@
+export const users = [
+  {
+    userImage: 'https://avatars.githubusercontent.com/u/62706988?v=4',
+    userName: 'Jamie',
+  },
+  {
+    userImage: 'https://avatars.githubusercontent.com/u/93566353?v=4',
+    userName: 'Khan',
+  },
+];
+
+export const labels = [
+  {
+    labelName: 'Document',
+    labelColor: '#000000',
+    textColor: '#ffffff',
+  },
+  {
+    labelName: 'Milestone',
+    labelColor: '#000000',
+    textColor: '#ffffff',
+  },
+];
+
+export let milestones: {
+  name: string;
+  ratio: string;
+}[];
