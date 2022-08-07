1d553edd733bc6d4696354b3cedddd13e9a7d5ae
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-08-08 03:26:37 +0900

    feat : 마일스톤 목 데이터  (mina-gwak/issue-tracker#52)

---
+++ FE/src/data/milestoneData.ts
@@ -0,0 +1,10 @@
+import { MilestoneTabType } from '@type/milestone';
+
+export const defaultMilestoneData: MilestoneTabType = {
+  id: 0,
+  name: '마일스톤 이름',
+  description: '설명(선택)',
+  dueDate: '완료일 (양식 : YYYY-MM-DD)',
+  openIssueCount: 3,
+  closeIssueCount: 3,
+};
