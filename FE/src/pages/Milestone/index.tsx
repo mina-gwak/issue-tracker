a40ca07ca5c9788dd1111a2a693a127e89a9a8fb
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-08-08 03:15:01 +0900

    feat : 마일스톤 페이지 (mina-gwak/issue-tracker#52)

--- FE/src/pages/Milestone/index.tsx
+++ FE/src/pages/Milestone/index.tsx
@@ -1,5 +1,42 @@
-const Milestone = () => {
-  return <div>마일스톤 페이지</div>;
-};
+import { useEffect, useState } from 'react';
 
+import { useSetRecoilState } from 'recoil';
+import styled from 'styled-components';
+
+import MilestoneCreateEditForm from '@components/MilestoneCreateEditForm';
+import MilestoneTable from '@components/MilestoneTable';
+import TabsPageHeader from '@components/TabsPageHeader';
+import { labelMilestoneActiveState } from '@store/labelMilestoneActive';
+
+const Milestone = () => {
+  const [isCreateMilestone, setIsCreateMilestone] = useState(false);
+  const setLabelMilestoneActiveValue = useSetRecoilState(labelMilestoneActiveState);
+
+  const handelClickCancel = () => setIsCreateMilestone(false);
+
+  useEffect(() => {
+    setLabelMilestoneActiveValue({ label: false, milestone: true });
+  }, []);
+
+  return (
+    <Wrapper>
+      <TabsPageHeader isCreateTabs={isCreateMilestone} setIsCreateTabs={setIsCreateMilestone} />
+      {isCreateMilestone && (
+        <MilestoneCreateEditForm
+          title='새로운 마일스톤 생성'
+          type='create'
+          setEditMode={handelClickCancel}
+        />
+      )}
+      <MilestoneTable />
+    </Wrapper>
+  );
+};
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1440px;
+  padding: 0 80px;
+  margin: 0 auto 120px;
+`;
 export default Milestone;
