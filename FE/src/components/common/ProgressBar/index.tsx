88e3adf633572d43f71845df455ecba550029da6
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-08-08 03:03:03 +0900

    feat : material-ui를 통한 프로그래스 바 (mina-gwak/issue-tracker#52)

---
+++ FE/src/components/common/ProgressBar/index.tsx
@@ -0,0 +1,39 @@
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { makeStyles, createStyles, withStyles, Theme } from '@material-ui/core/styles';
+
+const BorderLinearProgress = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      height: 10,
+      borderRadius: 5,
+    },
+    colorPrimary: {
+      backgroundColor: theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
+    },
+    bar: {
+      borderRadius: 5,
+      backgroundColor: '#1a90ff',
+    },
+  }),
+)(LinearProgress);
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+    paddingTop: 14,
+    paddingBottom: 14,
+  },
+});
+
+interface ProgressBarPropsType {
+  progress: number;
+}
+export default function ProgressBar({ progress }: ProgressBarPropsType) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <BorderLinearProgress variant='determinate' value={progress} />
+    </div>
+  );
+}
