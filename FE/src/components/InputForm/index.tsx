a485db2df03efddf7fcdfecf0c4979d3a40b8a96
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-08-08 03:04:21 +0900

    feat : 불필요한 props 제거 (mina-gwak/issue-tracker#52)

--- FE/src/components/InputForm/index.tsx
+++ FE/src/components/InputForm/index.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 
 interface InputFormType {
   placeholder?: string;
-  value?: string;
   inputValue: string;
   onChange?: ({ target }: { target: HTMLInputElement }) => void;
   children?: ReactNode;
