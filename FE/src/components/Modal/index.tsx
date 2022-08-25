e4aaa20fb2511ab505783b4bccdc8b535802de7b
mina-gwak <mina.gwak@kakao.com>
2022-08-26 03:06:27 +0900

    refactor: useModal 훅 생성 및 적용, useOptions 훅 적용 (mina-gwak/issue-tracker#69)

--- FE/src/components/Modal/index.tsx
+++ FE/src/components/Modal/index.tsx
@@ -1,126 +1,38 @@
-import { RefObject, useEffect } from 'react';
+import { RefObject } from 'react';
 
-import axios from 'axios';
-import qs from 'qs';
-import { useRecoilValue, useRecoilState } from 'recoil';
-
-import { IconsType } from '@assets/icons';
 import * as S from '@components/Modal/Modal.style';
 import { POSITION } from '@components/Modal/constants';
 import Icon from '@components/common/Icon';
-import { ICON_NAME } from '@components/common/Icon/constants';
 import Image from '@components/common/Image';
 import { IMAGE_SIZE } from '@components/common/Image/constants';
-import { API } from '@constants';
-import useCheckBox from '@hooks/useCheckBox';
-import { queryClient } from '@src';
-import { filterBarArrState, filterBarState } from '@store/filterBar';
+import { ModalStateType } from '@store/dropdown';
 import { DropdownType } from '@type/dropdownType';
+import { checkBoxClickHandlerType, isCheckedType } from '@type/useOption';
+
+export type FilterType = ModalStateType | 'is';
 
 export interface ModalPropsType {
   modalRef: RefObject<HTMLDivElement>;
   data?: DropdownType[];
   title: string;
-  type: string;
   position?: string;
-  handleModalClick: (event: MouseEvent) => void;
+  isChecked: isCheckedType;
+  checkBoxClickHandler: checkBoxClickHandlerType;
 }
-
-interface CheckedListType {
-  id: number[];
-  status: string;
-}
-
-type isCheckedType = {
-  (value?: string, optionName?: string): IconsType;
-};
-
-type checkBoxClickHandlerType = {
-  (value?: string, optionName?: string): () => void;
-};
 
 const Modal = ({
   modalRef,
   data,
   title,
-  type,
   position = POSITION.LEFT,
-  handleModalClick,
+  isChecked,
+  checkBoxClickHandler,
 }: ModalPropsType) => {
-  const filterBarArrValue = useRecoilValue(filterBarArrState);
-  const [filterBarValue, setFilterBarValue] = useRecoilState(filterBarState);
-  const { isCheckedItems, resetCheckedItems } = useCheckBox();
-
-  const isChecked: isCheckedType = (value) => {
-    let checkBoxIcon: IconsType = ICON_NAME.CHECKBOX_CIRCLE_INITIAL;
-
-    if (!value) return checkBoxIcon;
-
-    for (const [objKey, objValue] of filterBarArrValue) {
-      if (Array.isArray(objValue)) {
-        if (objKey === type) {
-          objValue.forEach((item: string) => {
-            if (item === value) checkBoxIcon = ICON_NAME.CHECKBOX_CIRCLE_ACTIVE;
-          });
-        }
-      }
-    }
-    return checkBoxIcon;
-  };
-
-  const checkBoxClickHandler: checkBoxClickHandlerType = (value) => () => {
-    if (!value) return;
-    for (const [, objValue] of filterBarArrValue) {
-      if (Array.isArray(objValue)) {
-        const checkSameValue = objValue.includes(value);
-        if (type === 'is') {
-          setFilterBarValue({ ...filterBarValue, [type]: ['issue', value] });
-        } else if (type === 'stateModify') {
-          fetchStateModify(value);
-        } else {
-          if (!checkSameValue) {
-            setFilterBarValue({ ...filterBarValue, [type]: [value] });
-          }
-          if (checkSameValue) {
-            setFilterBarValue({ ...filterBarValue, [type]: [] });
-          }
-        }
-      }
-    }
-  };
-
-  const fetchStateModify = async (value: string) => {
-    const accessToken = localStorage.getItem('accessToken')!;
-    const checkedList = { id: Array.from(isCheckedItems), status: value };
-
-    const myAxios = axios.create({
-      paramsSerializer: (checkedList: CheckedListType) =>
-        qs.stringify(checkedList, { arrayFormat: 'repeat' }),
-    });
-
-    await myAxios.post(`${API.STATUS_UPDATE()}`, null, {
-      headers: {
-        Authorization: `Bearer ${JSON.parse(accessToken)}`,
-      },
-      params: checkedList,
-    });
-
-    queryClient.invalidateQueries('issues');
-    resetCheckedItems();
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', handleModalClick, true);
-
-    return () => {
-      document.removeEventListener('click', handleModalClick, true);
-    };
-  }, [modalRef]);
-
   return (
     <S.MenuList position={position} ref={modalRef}>
-      <S.MenuTitle>{title} 필터</S.MenuTitle>
-      {data &&
+      <S.MenuTitle>{title}</S.MenuTitle>
+      <S.MenuOptionList>
+        {data &&
         data.map(({ optionName, value, imageUrl, colorCode }) => (
           <S.MenuOptionGroup key={optionName}>
             <S.MenuOptionItem>
@@ -128,11 +40,12 @@ const Modal = ({
               {colorCode && <S.CircleColorIcon colorIcon={colorCode} />}
               <S.MenuItemOption>{optionName}</S.MenuItemOption>
             </S.MenuOptionItem>
-            <button onClick={checkBoxClickHandler(value || optionName)}>
+            <button type='button' onClick={checkBoxClickHandler(value || optionName)}>
               <Icon iconName={isChecked(value || optionName)} />
             </button>
           </S.MenuOptionGroup>
         ))}
+      </S.MenuOptionList>
     </S.MenuList>
   );
 };
--- FE/src/components/SideBar/SideBarSection/index.tsx
+++ FE/src/components/SideBar/SideBarSection/index.tsx
@@ -1,22 +1,61 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useRef, useState } from 'react';
 
+import Modal from '@components/Modal';
+import { POSITION } from '@components/Modal/constants';
 import * as S from '@components/SideBar/SideBarSection/SideBarSection.style';
 import Icon from '@components/common/Icon';
 import { ICON_NAME, ICON_SIZE } from '@components/common/Icon/constants';
+import useIssueOptions from '@hooks/useIssueOptions';
+import useModal from '@hooks/useModal';
+import { useLabelsQuery } from '@query/label';
+import { useMilestonesQuery } from '@query/milestone';
+import { useUsersQuery } from '@query/user';
+import { DropdownType } from '@type/dropdownType';
 
 interface SideBarSectionPropsType {
-  title: string;
+  title: '담당자' | '레이블' | '마일스톤';
+  type: 'assignees' | 'labels' | 'milestone';
   children?: ReactNode;
 }
 
-const SideBarSection = ({ title, children }: SideBarSectionPropsType) => {
+const SideBarSection = ({ title, children, type }: SideBarSectionPropsType) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+  const [modalData, setModalData] = useState<DropdownType[]>();
+  const [trigger, setTrigger] = useState<string>('');
+
+  const [isModalOpen, toggleModal] = useModal({ modalRef });
+  const { isChecked, checkBoxClickHandler } = useIssueOptions({ type });
+
+  const { data: assignees } = useUsersQuery(trigger);
+  const { data: labels } = useLabelsQuery(trigger);
+  const { data: milestones } = useMilestonesQuery(trigger);
+
+  useEffect(() => {
+    if (type === 'assignees') assignees && setModalData(assignees);
+    else if (type === 'labels')
+      labels &&
+        setModalData(labels.map(({ name, colorCode }) => ({ optionName: name, colorCode })));
+    else if (type === 'milestone')
+      milestones && setModalData(milestones.map(({ name }) => ({ optionName: name })));
+  }, [assignees, labels, milestones]);
+
   return (
     <S.Container>
-      <S.AddButton>
+      <S.AddButton type='button' onMouseEnter={() => setTrigger(type)} onClick={toggleModal}>
         <S.Title>{title}</S.Title>
         <Icon iconName={ICON_NAME.ADD} iconSize={ICON_SIZE.SMALL} />
       </S.AddButton>
       {children}
+      {isModalOpen && (
+        <Modal
+          modalRef={modalRef}
+          data={modalData}
+          title={`${title} 추가`}
+          position={POSITION.LEFT}
+          isChecked={isChecked}
+          checkBoxClickHandler={checkBoxClickHandler}
+        />
+      )}
     </S.Container>
   );
 };
