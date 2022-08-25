3de6866f409d0e36575ae4c78124a0dec9aa309a
mina-gwak <mina.gwak@kakao.com>
2022-08-26 03:10:57 +0900

    feat: 이슈 추가 폼 컴포넌트 생성 (mina-gwak/issue-tracker#69)

--- FE/src/pages/AddIssue/index.tsx
+++ FE/src/pages/AddIssue/index.tsx
@@ -1,43 +1,13 @@
-import { useState } from 'react';
-
-import { useRecoilValue } from 'recoil';
-
-import SideBar from '@components/SideBar';
-import Button from '@components/common/Button';
-import { BUTTON_SIZE } from '@components/common/Button/constants';
-import Image from '@components/common/Image';
-import { IMAGE_SIZE } from '@components/common/Image/constants';
-import TextInput from '@components/common/TextInput';
-import { INPUT_SIZE } from '@components/common/TextInput/constants';
-import Textarea from '@components/common/Textarea';
+import AddIssueForm from '@components/AddIssueForm';
 import * as S from '@pages/AddIssue/AddIssue.style';
-import { userState } from '@store/user';
 
 const AddIssue = () => {
-  const [comment, setComment] = useState('');
-
-  const { name, image } = useRecoilValue(userState);
 
   return (
     <S.Wrapper>
       <S.PageTitle>새로운 이슈 작성</S.PageTitle>
       <S.Divider />
-      <S.Form>
-        <S.FormContainer>
-          <S.FormWrapper>
-            <Image url={image} alt={name} size={IMAGE_SIZE.MEDIUM} />
-            <S.FormElements>
-              <TextInput size={INPUT_SIZE.MEDIUM} name='title' placeholder='제목' />
-              <Textarea comment={comment} setComment={setComment} />
-            </S.FormElements>
-          </S.FormWrapper>
-          <SideBar />
-        </S.FormContainer>
-        <S.Divider />
-        <Button size={BUTTON_SIZE.MEDIUM} onClick={() => {}}>
-          완료
-        </Button>
-      </S.Form>
+      <AddIssueForm />
     </S.Wrapper>
   );
 };
