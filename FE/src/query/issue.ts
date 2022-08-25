dea72ec141981bbda08018768f2cf8ce5ef26e24
mina-gwak <mina.gwak@kakao.com>
2022-08-26 03:10:31 +0900

    feat: 이슈 추가 요청 함수 추가 (mina-gwak/issue-tracker#69)

--- FE/src/query/issue.ts
+++ FE/src/query/issue.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useQuery } from 'react-query';
 
 import { API } from '@constants';
-import { IssueDataType } from '@type/issueType';
+import { AddIssueDataType, IssueDataType } from '@type/issueType';
 
 type IssueQueryPropsType<T> = (data: IssueDataType) => T;
 
@@ -19,6 +19,18 @@ export const fetchIssues = async <T>(filterBarValue: string): Promise<T> => {
   });
 
   return response.data;
+};
+
+export const addIssue = async (newIssue: AddIssueDataType): Promise<AddIssueDataType> => {
+  const accessToken = localStorage.getItem('accessToken')!;
+
+  const { data } = await axios.post(`${API.ISSUE}`, newIssue, {
+    headers: {
+      Authorization: `Bearer ${JSON.parse(accessToken)}`,
+    },
+  });
+
+  return data;
 };
 
 export const useIssuesQuery = <T>(filterBarValue: string, select?: IssueQueryPropsType<T>) =>
