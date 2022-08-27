86c4a8b7371894215d636e355d5a9b39439fa5ed
leejohy-0223 <leejohy@naver.com>
2022-08-27 16:03:04 +0900

    fix: redis에서 localdatetime에 대한 (de)serialization 방법 지정

--- BE/src/main/java/com/codesquad/issueTracker/issue/application/dto/PopUpResponse.java
+++ BE/src/main/java/com/codesquad/issueTracker/issue/application/dto/PopUpResponse.java
@@ -3,14 +3,24 @@ package com.codesquad.issueTracker.issue.application.dto;
 import java.time.LocalDateTime;
 
 import com.codesquad.issueTracker.issue.domain.Issue;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PopUpResponse {
 
     private String title;
     private String content;
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime writtenTime;
     private boolean assignedMe;
 
@@ -22,8 +32,5 @@ public class PopUpResponse {
         if (this.content.length() > 50) {
             this.content = this.content.substring(0, 50) + "...";
         }
-    }
-
-    public PopUpResponse() {
     }
 }
