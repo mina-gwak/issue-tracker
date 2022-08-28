ce360e23220ac7e6bd66ae468ce790e62f4a91d9
mina-gwak <mina.gwak@kakao.com>
2022-08-29 04:07:26 +0900

    refactor: API 형식 수정 (mina-gwak/issue-tracker#69)

--- FE/src/constants/api.ts
+++ FE/src/constants/api.ts
@@ -1,6 +1,7 @@
 export const API = {
   GITHUB_LOGIN_CALLBACK: '/api/oauth/login/github/callback',
   ISSUE: '/api/issues',
+  ISSUE_DELETE: (issueId: number) => `/api/issues/${issueId}`,
   ISSUE_POPOVER: (issueId: number) => `/api/issues/${issueId}/popUp`,
   FILTER: (type: string) => `/api/${type}`,
   STATUS_UPDATE: `/api/issues/status`,
