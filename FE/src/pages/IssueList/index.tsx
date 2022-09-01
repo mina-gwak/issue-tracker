eb39fca5e2af599b8be18aadada672514facec43
mina-gwak <mina.gwak@kakao.com>
2022-09-02 05:02:05 +0900

    refactor: 이슈 데이터 로딩 처리 추가 (mina-gwak/issue-tracker#81)

--- FE/src/pages/IssueList/index.tsx
+++ FE/src/pages/IssueList/index.tsx
@@ -1,8 +1,18 @@
+import { useRecoilValue } from 'recoil';
+
 import IssueMenu from '@components/IssueMenu';
 import IssueTable from '@components/IssueTable';
 import * as S from '@pages/IssueList/IssueList.style';
+import Loading from '@pages/Loading';
+import { useIssuesQuery } from '@query/issue';
+import { filterBarQueryString } from '@store/filterBar';
 
 const IssueList = () => {
+  const filterBarValue = useRecoilValue(filterBarQueryString);
+  const { isLoading } = useIssuesQuery(filterBarValue);
+
+  if (isLoading) return <Loading />;
+
   return (
     <S.Wrapper>
       <IssueMenu />
